92e85530052bcf9fac22aa659ec45305a2d4ce71
Paulo Padrao <55985904+pvpadrao@users.noreply.github.com>
2023-02-08 15:39:24 -0500

    Add files via upload

---
+++ my_nav2_pkg/my_nav2_pkg/nav2_test.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import rclpy
+from nav2_simple_commander.robot_navigator import BasicNavigator
+from geometry_msgs.msg import PoseStamped
+import tf_transformations
+
+def create_pose_stamped(navigator: BasicNavigator, position_x, position_y, orientation_z):
+    q_x, q_y, q_z, q_w = tf_transformations.quaternion_from_euler(0.0, 0.0, orientation_z)
+    pose = PoseStamped()
+    pose.header.frame_id = 'map'
+    pose.header.stamp = navigator.get_clock().now().to_msg()
+    pose.pose.position.x = position_x
+    pose.pose.position.y = position_y
+    pose.pose.position.z = 0.0
+    pose.pose.orientation.x = q_x
+    pose.pose.orientation.y = q_y
+    pose.pose.orientation.z = q_z
+    pose.pose.orientation.w = q_w
+    return pose
+
+def main(args=None):
+    rclpy.init(args=args)
+    nav = BasicNavigator()
+
+    #--- Set Initial Pose
+    q_x, q_y, q_z, q_w = tf_transformations.quaternion_from_euler(0.0, 0.0, 0.0)
+    initial_pose = PoseStamped()
+    initial_pose.header.frame_id = 'map'
+    initial_pose.header.stamp = nav.get_clock().now().to_msg()
+    initial_pose.pose.position.x = 0.0
+    initial_pose.pose.position.y = 0.0
+    initial_pose.pose.position.z = 0.0
+    initial_pose.pose.orientation.x = q_x
+    initial_pose.pose.orientation.y = q_y
+    initial_pose.pose.orientation.z = q_z
+    initial_pose.pose.orientation.w = q_w
+    nav.setInitialPose(initial_pose)
+
+    #--- Wait for Nav2
+    nav.waitUntilNav2Active()
+
+    #--- Set Goal
+    # pi == 3.14 == 180 
+    # pi/2 == 1.57 = 90
+    goal_pose1 = create_pose_stamped(nav, 3.5, 1.0, 1.57)
+    goal_pose2 = create_pose_stamped(nav, 2.0, 2.5, 3.14)
+    goal_pose3 = create_pose_stamped(nav, 0.5, 1.0, -1.57)
+    
+    # --- Go to one pose
+    # nav.goToPose(goal_pose1)
+    # while not nav.isTaskComplete():
+    #   feedback = nav.getFeedback()
+
+    # --- follow waypoints
+    waypoints = [goal_pose1, goal_pose2, goal_pose3]
+    nav.followWaypoints(waypoints)
+    while not nav.isTaskComplete():
+        feedback = nav.getFeedback()
+
+    print(nav.getResult())
+    #---
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
---
+++ my_nav2_pkg/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'my_nav2_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='paulopadrao',
+    maintainer_email='pv.padrao@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        "nav2_test = my_nav2_pkg.nav2_test:main"
+        ],
+    },
+)
